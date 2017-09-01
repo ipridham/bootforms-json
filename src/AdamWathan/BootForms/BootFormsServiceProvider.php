ec0edacf24e66de4121e0c9bc56a52cb8eac60e6
mrterryh <mrterrymh@gmail.com>
2017-09-01 15:15:39 +0100

    Rename deprecated methods

--- src/AdamWathan/BootForms/BootFormsServiceProvider.php
+++ src/AdamWathan/BootForms/BootFormsServiceProvider.php
@@ -32,21 +32,21 @@ class BootFormsServiceProvider extends ServiceProvider
 
     protected function registerErrorStore()
     {
-        $this->app['adamwathan.form.errorstore'] = $this->app->share(function ($app) {
+        $this->app['adamwathan.form.errorstore'] = $this->app->register(function ($app) {
             return new IlluminateErrorStore($app['session.store']);
         });
     }
 
     protected function registerOldInput()
     {
-        $this->app['adamwathan.form.oldinput'] = $this->app->share(function ($app) {
+        $this->app['adamwathan.form.oldinput'] = $this->app->register(function ($app) {
             return new IlluminateOldInputProvider($app['session.store']);
         });
     }
 
     protected function registerFormBuilder()
     {
-        $this->app['adamwathan.form'] = $this->app->share(function ($app) {
+        $this->app['adamwathan.form'] = $this->app->register(function ($app) {
             $formBuilder = new FormBuilder;
             $formBuilder->setErrorStore($app['adamwathan.form.errorstore']);
             $formBuilder->setOldInputProvider($app['adamwathan.form.oldinput']);
@@ -58,21 +58,21 @@ class BootFormsServiceProvider extends ServiceProvider
 
     protected function registerBasicFormBuilder()
     {
-        $this->app['bootform.basic'] = $this->app->share(function ($app) {
+        $this->app['bootform.basic'] = $this->app->register(function ($app) {
             return new BasicFormBuilder($app['adamwathan.form']);
         });
     }
 
     protected function registerHorizontalFormBuilder()
     {
-        $this->app['bootform.horizontal'] = $this->app->share(function ($app) {
+        $this->app['bootform.horizontal'] = $this->app->register(function ($app) {
             return new HorizontalFormBuilder($app['adamwathan.form']);
         });
     }
 
     protected function registerBootForm()
     {
-        $this->app['bootform'] = $this->app->share(function ($app) {
+        $this->app['bootform'] = $this->app->register(function ($app) {
             return new BootForm($app['bootform.basic'], $app['bootform.horizontal']);
         });
     }
